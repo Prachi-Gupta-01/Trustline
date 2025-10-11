d67b95861db229e2dcb291f9cdee6d323f537e50
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2025-10-11 09:44:35 +0530

    updated

--- trustFront/my-app/src/pages/Home/Home.jsx
+++ trustFront/my-app/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Features from "../../component/features/features";
 import Footer from "../../component/Footer/Footer";
 
 import FAQSection from "../../component/FAQ/FAQ";
-import AboutMe from "../aboutme/aboutme";
+
 
 
 
@@ -17,7 +17,7 @@ const Home=()=> {
             <Features/>
             <FAQSection/>   
             <Footer/>
-            <AboutMe/>
+        
         </div>
     )
 }
